f41a4537662acbe243999221163a34bb4c0e13c4
mbreslawskij <mbreslawskij@gmail.com>
2021-09-02 22:57:05 -0300

    Add project files.

---
+++ CSharp.Capitulo01.Sintaxe/VariaveisForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.Capitulo01.Sintaxe
+{
+    public partial class VariaveisForm : Form
+    {
+        int x = 32;
+        int y = 16;
+        int w = 45;
+        int z = 32;
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VariaveisForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        // estemétodo é usado para atender
+        private void aritmeticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultadoListBox.Items.Clear();
+            //tipos de dados: pagina 25
+
+            int x = 42;
+            int X = 10;
+
+            long população = 1212312312312312312;
+            string nome = "Marcio", sobrenome = "Breslawskij";
+            x = 5;
+            //X = "14";//estaticamente tipada;
+            char letra = 'a';
+            bool aprovado = true;
+            DateTime dataNascimento = new DateTime(1983, 10, 15);
+            var dataNascimentos = new DateTime(1983, 10, 15);
+            decimal valor = 13.6m;
+            double outroValor = 2.2;
+            float distancia = 100.32f;
+            decimal media = 8.0M;
+            decimal bim1 = 5.6m;
+
+            object camaleao = "Marcio";
+            camaleao = 5;
+            camaleao = DateTime.Now;
+
+            int a = 2;
+            var b = 6;
+            var c = 0;
+            var d = 13;
+            var f = 5.6m;
+
+            var outroNome = "Breslawskij";
+
+            decimal preco = 14;
+            //preco = 25;
+
+            resultadoListBox.Items.Add("a = " + a); //Shift + Ctrl + barra de espaços
+            resultadoListBox.Items.Add(string.Concat("b = ", b));
+            resultadoListBox.Items.Add(string.Format("c = {0} ", c));
+            resultadoListBox.Items.Add($"d = {d}");
+
+            resultadoListBox.Items.Add($"c * d = {c * d}");
+            resultadoListBox.Items.Add($"d / a = {d / a}");
+            resultadoListBox.Items.Add($"d % a = {d % a}");
+
+
+        }
+
+        private void resultadoListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reduzidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultadoListBox.Items.Clear();
+
+            var x = 5;
+            resultadoListBox.Items.Add($"x = {x}");
+            resultadoListBox.Items.Add("----------------------------------------------");
+            //x = x - 3;
+            x -= 3;
+            resultadoListBox.Items.Add($"x = {x}");
+        }
+
+        private void incrementaisDecrementaisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultadoListBox.Items.Clear();
+
+            int a;
+
+            a = 5;
+            resultadoListBox.Items.Add("Exemplo de pré-incremental");
+            resultadoListBox.Items.Add($"a = {a}");
+            resultadoListBox.Items.Add($"2 + ++a = {2 + ++a}");
+            resultadoListBox.Items.Add($"a = {a}");
+
+            resultadoListBox.Items.Add(new string('-', 50));
+
+            a = 5;
+            resultadoListBox.Items.Add("Exemplo de pós-incremental");
+            resultadoListBox.Items.Add($"a = {a}");
+            resultadoListBox.Items.Add($"2 + a++ = {2 + a++}");
+            resultadoListBox.Items.Add($"a = {a}");
+
+        }
+
+        private void booleanasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExibiValores();
+
+            resultadoListBox.Items.Add($"w <= x = {w <= x}");
+            resultadoListBox.Items.Add($"x == z = {x == z}");
+            resultadoListBox.Items.Add($"x != z = {x != z}");
+
+
+        }
+
+        private void ExibiValores()
+        {
+            resultadoListBox.Items.Clear();
+
+            resultadoListBox.Items.Add($"x = {x}");
+            resultadoListBox.Items.Add($"y = {y}");
+            resultadoListBox.Items.Add($"w = {w}");
+            resultadoListBox.Items.Add($"z = {z}");
+
+            resultadoListBox.Items.Add(new string('-', 50));
+        }
+
+        private void logicasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //if (true)
+            //{
+            //    var x = 10;
+            //}
+
+            //x = 5;
+
+            resultadoListBox.Items.Add($"w <= x || y == 16 = {w <= x || y == 16}");
+            resultadoListBox.Items.Add($"x == z && x != z = {x == z && x != z}");
+            resultadoListBox.Items.Add($"!(y > w) = {!(y > w)}");
+        }
+    }
+}
+
---
+++ ProgramandoComCSharpSet21/CSharp.Capitulo01.Sintaxe/VariaveisForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace CSharp.Capitulo01.Sintaxe
+{
+    partial class VariaveisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.operaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aritmeticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultadoListBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operaçõesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1030, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // operaçõesToolStripMenuItem
+            // 
+            this.operaçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aritmeticasToolStripMenuItem});
+            this.operaçõesToolStripMenuItem.Name = "operaçõesToolStripMenuItem";
+            this.operaçõesToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.operaçõesToolStripMenuItem.Text = "&Operações";
+            // 
+            // aritmeticasToolStripMenuItem
+            // 
+            this.aritmeticasToolStripMenuItem.Name = "aritmeticasToolStripMenuItem";
+            this.aritmeticasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aritmeticasToolStripMenuItem.Text = "&Aritméticas";
+            this.aritmeticasToolStripMenuItem.Click += new System.EventHandler(this.aritmeticasToolStripMenuItem_Click);
+            // 
+            // resultadoListBox
+            // 
+            this.resultadoListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultadoListBox.FormattingEnabled = true;
+            this.resultadoListBox.Location = new System.Drawing.Point(0, 24);
+            this.resultadoListBox.Name = "resultadoListBox";
+            this.resultadoListBox.Size = new System.Drawing.Size(1030, 560);
+            this.resultadoListBox.TabIndex = 1;
+            // 
+            // VariaveisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 584);
+            this.Controls.Add(this.resultadoListBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VariaveisForm";
+            this.Text = "Variáveis e operações";
+            this.Load += new System.EventHandler(this.VariaveisForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem operaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aritmeticasToolStripMenuItem;
+        private System.Windows.Forms.ListBox resultadoListBox;
+    }
+}
+
